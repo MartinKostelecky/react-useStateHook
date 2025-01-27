c67f5336829f7f6eeca14c7faf9859092cf3bcba
MartinKostelecky <kosteleckymartin@gmail.com>
2025-01-27 12:42:19 +0100

    updateEmployed modified based on actual date + css added

--- src/Details.jsx
+++ src/Details.jsx
@@ -2,12 +2,13 @@ import React, {useState} from "react";
 
 function Details() {
 
+    const date = new Date();
     const [name, setName] = useState("Guest");
     const [age, setAge] = useState(0);
-    const [isEmployed, setIsEmployed] = useState(false);
+    const [isEmployed, setIsEmployed] = useState(null);
 
     const updateName = () => {
-        let name = "Santa Claus"
+        let name = "Santa Claus";
         setName(name);
         console.log(name);
     }
@@ -18,25 +19,26 @@ function Details() {
         console.log(age);
     }
 
-    //TODO change to employed only on 24th of December
     const updateEmployed = () => {
-        let isEmployed = false;
-        setIsEmployed(!isEmployed);
+        let isEmployed = date.getUTCDate() === 24 && date.getMonth() === 11;
+        setIsEmployed(isEmployed);
+        console.log(date.getDate());
+        console.log(date.getMonth());
         console.log(isEmployed);
     }
 
-    return(
-        <div>
-            <p>Name: {name} </p>
+    return (
+        <div className="container">
+            <p className="text">Name: {name}</p>
             <button onClick={updateName}>Set name</button>
 
-            <p>Age: {age}</p>
+            <p className="text">Age: {age}</p>
             <button onClick={updateAge}>Set age</button>
 
-            <p>Employed: {isEmployed ? "Yes" : "No"}</p>
-            <button onClick={updateEmployed}>Set employed</button>
+            <p className="text">Employed: {isEmployed === null ? "" : isEmployed ? "Yes" : "No"}</p>
+            <button onClick={updateEmployed}>Show is employed</button>
         </div>
-    )
+    );
 
 }
 
